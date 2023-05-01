268db097d65b67df201dedab6dfe523ee8071e2f
Ngab Owi <130557632+XyzonXD@users.noreply.github.com>
2023-05-02 06:10:09 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ from src import cok as yy
 if __name__ == "__main__":
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
     yy.cek_server()
+    print(" Server Sedang Maintenance")
+    print(" Sihlah kann tunggu beberapa saat lgi")
+    print(" Code By Xyzon Dev")
